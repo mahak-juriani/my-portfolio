b693eb5ade93e27e5c0d31b94510bb5083005f9e
Mahak Juriani <juriani.mahak@gmail.com>
2024-06-02 21:45:43 +0530

    +education component, certifications update

---
+++ src/Pages/Education/Education.jsx
@@ -0,0 +1,25 @@
+import './Education.css';
+function Education() {
+    return (
+        <div class="timelines">
+            <div class="timeline education">
+                <h2 class="timeline-title">Education</h2>
+                <br />
+                <div class="timeline-items">
+                    <div class="timeline-item">
+                        <h3>Bachelor's of Science in Computer Science</h3>
+                        <time datetime="2023/2027">2033 &ndash; 2027</time>
+                        <div class="location">Scaler School of Technology, Bangalore</div>
+                    </div>
+                    <div class="timeline-item">
+                        <h3>St. Joseph's Convent Girls' Sr. Sec. School</h3>
+                        <time datetime="2009/2023">2009 &ndash; 2023</time>
+                        <div class="location">Jabalpur, Madhya Pradesh</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Education;
